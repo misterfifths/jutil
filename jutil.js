51050f0a3d41b0344b4147f01129c61aee3cad69
Tim Clem <tim.clem@gmail.com>
2012-01-21 15:20:53 -0500

    Add jsort command

--- jutil.js
+++ jutil.js
@@ -269,6 +269,32 @@ if(require.main == module) {
             needsSandbox: true,
             hasWithClauseOpt: true,
             handler: formatCommandHandler
+        },
+
+        sort: {
+            help: 'Sort the objects in the input by a given key expression.',
+            options: {
+                sortKeyExpr: {
+                    position: 1,
+                    help: 'The expression that provides the sort key for each object in the loaded data. If omitted, defaults to the object itself.'
+                },
+                ignoreCase: {
+                    abbr: 'i',
+                    full: 'ignore-case',
+                    flag: true,
+                    help: 'Ignore case when comparing string sort keys.'
+                },
+                descending: {
+                    abbr: 'r',
+                    full: 'reverse',
+                    flag: true,
+                    help: 'Reverse the result of comparisons; output objects in descending order.'
+                }
+            },
+            outputsObject: true,
+            needsSandbox: true,
+            hasWithClauseOpt: true,
+            handler: sortCommandHandler
         }
     });
 }
@@ -617,6 +643,45 @@ function formatCommandHandler(runtimeSettings, config, opts)
     }
 
     return res;
+}
+
+
+//// Sort command
+
+function sortCommandHandler(runtimeSettings, config, opts)
+{
+    var vm = require('vm'),
+        data = runtimeSettings.data,
+        keyedData = [],
+        i,
+        expr = opts.sortKeyExpr || 'this';  // default sort key is the whole object
+    
+    if(!Array.isArray(data))
+        return data;
+    
+    // Generate keys and stash them in keyedData
+    mapOverInput(expr, runtimeSettings, function(obj, key) {
+        keyedData.push({ key: key, obj: obj });
+        return true;
+    });
+
+    // Sort keyedData on keys
+    keyedData.sort(function(x, y) {
+        if(opts.ignoreCase) {
+            if(typeof x.key == 'string') x.key = x.key.toLowerCase();
+            if(typeof y.key == 'string') y.key = y.key.toLowerCase();
+        }
+
+        if(x.key == y.key) return 0;
+        if(x.key < y.key) return opts.descending ? 1 : -1;
+        return opts.descending ? -1 : 1;
+    });
+
+    // Unwrap the objects in keyedData
+    for(i = 0; i < keyedData.length; i++)
+        data[i] = keyedData[i].obj;
+    
+    return data;
 }
 
 
